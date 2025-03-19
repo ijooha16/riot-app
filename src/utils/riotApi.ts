4e98622e695c8356af0e968187b796f29cbccb9b
Juha Yoon <ijooha16@gmail.com>
2025-03-19 18:02:44 +0900

    fix: build

--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,7 +1,7 @@
 import { Champion } from "@/types/Champion";
 
 export const getChampionRotation = async () => {
-  const res = await fetch("https://riot-kttc1oszs-juha-yoons-projects.vercel.app/api/rotation");
+  const res = await fetch("https://localhost:3000/api/rotation");
   const data: Champion[] = await res.json();
   return data;
 };
